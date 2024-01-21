1b042b382e6236a714daeff5a8ed5fcb0ad0f6b7
seyioo <81486074+seyiooo@users.noreply.github.com>
2024-01-21 20:05:58 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -8,7 +8,6 @@ module.exports = {
                 server();
                 channels();
                 roles();
-                messages();
                 dm();
             break;
             case 'server':
